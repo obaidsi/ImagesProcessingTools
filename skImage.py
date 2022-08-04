103fab34810c163c72a9a6ce1b0476080135e431
obaidsi <52484343+obaidsi@users.noreply.github.com>
2022-08-03 21:09:33 -0700

    Filtered noisy image

--- skImage.py
+++ skImage.py
@@ -6,6 +6,8 @@ Skimage Basics
 from skimage import io, color
 from skimage.transform import rescale, resize, downscale_local_mean
 from matplotlib import pyplot as plt
+from skimage.util import random_noise
+from skimage.filters import gaussian, sobel
 
 # When as_gray=True skimage convert the image values to float 0 o 1
 img = io.imread("C:/Users/MSSB2/Desktop/Image_Processing/Img_Projects/webb_Img/STScI-01G7DEE0V21GETVXYVT795AAV8.png", as_gray=True)
@@ -18,3 +20,12 @@ plt.imshow(img_resized)
 
 img_downscale = downscale_local_mean(img, (4, 3))
 plt.imshow(img_downscale)
+
+noisy_img = random_noise(img)
+plt.imshow(noisy_img, cmap='gray')
+
+filtered_img = gaussian(noisy_img)
+plt.imshow(filtered_img, cmap='gray')
+
+sobel_img = sobel(img)
+plt.imshow(sobel_img, cmap='gray')
